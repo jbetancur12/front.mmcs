b0ed6053f2555f86662a111654856c09345ed896
Jorge Betancur <jorge.betancur@teads.tv>
2024-03-12 22:48:27 -0500

    upload avatar

--- src/pages/Customer.tsx
+++ src/pages/Customer.tsx
@@ -1,8 +1,7 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import TableUsersCustomer from '../Components/TableUsersCustomer';
-
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import TableUsersCustomer from "../Components/TableUsersCustomer";
 
 // API URL
 const apiUrl = import.meta.env.VITE_API_URL;
@@ -95,66 +94,178 @@ const apiUrl = import.meta.env.VITE_API_URL;
 
 // export default CustomerDetail;
 
-interface UserData{
+interface UserData {
   nombre: string;
   email: string;
   telefono: string;
+  image?: string;
 }
 
-
 function UserProfile() {
-
   const { id } = useParams();
-  const [customerData, setCustomerData] = useState<UserData>({nombre: "", email:"", telefono: ""});
+  const [customerData, setCustomerData] = useState<UserData>({
+    nombre: "",
+    email: "",
+    telefono: "",
+    image: "",
+  });
+  const [file, setFile] = useState<File | null>(null);
+
+  const [image, setImage] = useState("/images/pngaaa.com-4811116.png");
   // Aquí puedes usar el valor de 'id' para cargar los detalles del cliente correspondiente
   // por ejemplo, hacer una solicitud a la API o acceder a tus datos.
 
   const getuserInfo = async () => {
-    const response = await axios.get(`${apiUrl}/customers/${id}`,{
+    const response = await axios.get(`${apiUrl}/customers/${id}`, {
       headers: {
-        'Authorization': `Bearer ${localStorage.getItem("accessToken")}`
-      }
-    })
+        Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    });
     if (response.status === 200) {
-      setCustomerData(response.data)
+      setCustomerData(response.data);
     }
+  };
 
-  }
+  const handleImageChange = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const newImage = event.target.files?.[0];
+    // Aquí puedes implementar la lógica para cargar la imagen al servidor y actualizarla en la base de datos
+    // También puedes utilizar librerías como axios para hacer la solicitud HTTP
+    if (newImage) {
+      setFile(newImage);
+      // const formData = new FormData();
+      // formData.append("file", newImage as Blob);
+      // formData.append(
+      //   "userId",
+      //   id !== undefined && typeof id === "number" ? id : ""
+      // );
 
-  useEffect(() =>{
-    getuserInfo()
-  },[])
+      // console.log("first", formData);
+      // try {
+      //   // Enviar la imagen al backend Express
+      //   const response = await axios.post(`${apiUrl}/customers`, formData, {
+      //     headers: {
+      //       "Content-Type": "multipart/form-data",
+      //       Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      //     },
+      //   });
+      //   console.log("Respuesta del backend:", response.data);
+
+      //   // Actualizar la imagen en el estado local
+      //   setImage(URL.createObjectURL(newImage));
+      // } catch (error) {
+      //   console.error("Error al enviar la imagen al backend:", error);
+      // }
+    } else {
+      setImage("/images/pngaaa.com-4811116.png");
+    }
+  };
+
+  const handleSaveImage = async () => {
+    const formData = new FormData();
+    formData.append("file", file as Blob);
+    formData.append(
+      "customerId",
+      id !== undefined && typeof id === "string" ? id : ""
+    );
+
+    console.log("first", Object.fromEntries(formData));
+    try {
+      // Enviar la imagen al backend Express
+      const response = await axios.post(`${apiUrl}/customers`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      });
+      console.log("Respuesta del backend:", response.data);
+
+      // Actualizar la imagen en el estado local
+    } catch (error) {
+      console.error("Error al enviar la imagen al backend:", error);
+    }
+  };
+
+  useEffect(() => {
+    getuserInfo();
+  }, []);
   return (
     <>
-    <div className="bg-white shadow-md rounded-lg p-8 max-w-md mx-auto mt-4">
-      <div className="text-center">
-        <img
-          src='/images/pngaaa.com-4811116.png' // Reemplaza con la URL de la imagen del usuario
-          alt={`${customerData.nombre}`}
-          className="w-24 h-24 rounded-full mx-auto"
-        />
-        <h2 className="text-2xl font-semibold mt-4">
-          {customerData.nombre}
-        </h2>
-      </div>
+      <div className="bg-white shadow-md rounded-lg p-8 max-w-md mx-auto mt-4">
+        <div className="text-center">
+          <div className="flex flex-col justify-center">
+            <label htmlFor="imageInput">
+              <img
+                src={file?.name} // Reemplaza con la URL de la imagen del usuario
+                alt={`${customerData.nombre}`}
+                className="w-24 h-24 rounded-full mx-auto cursor-pointer"
+              />
+            </label>
+            <input
+              type="file"
+              id="imageInput"
+              accept="image/*"
+              className="hidden"
+              onChange={handleImageChange}
+            />
+            {file && (
+              <button
+                onClick={handleSaveImage}
+                className="flex items-center justify-center self-center mt-3 w-fit px-4 py-1 bg-gray-200 text-gray-700 rounded-md hover:bg-gray-300 focus:outline-none focus:bg-gray-300"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-4 h-4 mr-2"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M14 2h-4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h4m5-5l1.41-1.41a2 2 0 0 0 0-2.83L19.83 6A2 2 0 0 0 18 6V4a2 2 0 0 0-2-2H7a2 2 0 0 0-2 2v2a2 2 0 0 0 2 2h1"
+                  />
+                </svg>
+                <span>Guardar</span>
+              </button>
+            )}
+          </div>
+          <h2 className="text-2xl font-semibold mt-4">{customerData.nombre}</h2>
+        </div>
 
-      <div className="mt-6">
-        {/* <h3 className="text-xl font-semibold">Información del usuario</h3> */}
-        <ul className="mt-3">
-          <li className="flex items-center text-gray-700">
-          <svg className="h-5 w-5 mr-2" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg>
-            {customerData.email}
-          </li>
-          <li className="flex items-center text-gray-700 mt-2">
-          <svg className="h-5 w-5 mr-2" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z"/></svg>
-            {customerData.telefono}
-          </li>
-        </ul>
+        <div className="mt-6">
+          {/* <h3 className="text-xl font-semibold">Información del usuario</h3> */}
+          <ul className="mt-3">
+            <li className="flex items-center text-gray-700">
+              <svg
+                className="h-5 w-5 mr-2"
+                xmlns="http://www.w3.org/2000/svg"
+                height="1em"
+                viewBox="0 0 512 512"
+              >
+                <path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z" />
+              </svg>
+              {customerData.email}
+            </li>
+            <li className="flex items-center text-gray-700 mt-2">
+              <svg
+                className="h-5 w-5 mr-2"
+                xmlns="http://www.w3.org/2000/svg"
+                height="1em"
+                viewBox="0 0 512 512"
+              >
+                <path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z" />
+              </svg>
+              {customerData.telefono}
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-    <div className="mt-5">
-    <TableUsersCustomer/>
-    </div>
+      <div className="mt-5">
+        <TableUsersCustomer />
+      </div>
     </>
   );
 }
