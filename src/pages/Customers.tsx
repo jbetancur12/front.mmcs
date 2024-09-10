baff1dfae684bbbc8377dd2aa076d4cf91077ca1
Jorge Betancur <jorge.betancur@teads.tv>
2024-09-10 10:51:17 -0500

    refactor(customers): removed unnecessary elements

--- src/pages/Customers.tsx
+++ src/pages/Customers.tsx
@@ -2,19 +2,7 @@ import React from 'react'
 import Table from '../Components/TableCustomers'
 
 const Users: React.FC = () => {
-  return (
-    <>
-      <div className='flex flex-col'>
-        <div className='overflow-x-auto'>
-          <div className='inline-block min-w-full align-middle'>
-            <div className='overflow-hidden shadow'>
-              <Table />
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  )
+  return <Table />
 }
 
 export default Users
