8e80f610f34f5037faddbc09476ea9e7c261aa83
Jorge Betancur <jabetancur12@gmail.com>
2025-10-10 18:34:37 -0500

    fix(vite): vite cofig

--- vite.config.ts
+++ vite.config.ts
@@ -1,42 +1,25 @@
-import { sentryVitePlugin } from '@sentry/vite-plugin'
+// import { sentryVitePlugin } from '@sentry/vite-plugin'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     nodePolyfills(),
-    tsconfigPaths(),
-    visualizer({
-      open: false, // No abrir automáticamente durante build
-      filename: 'stats.html',
-      gzipSize: true,
-      brotliSize: true
-    }),
-    sentryVitePlugin({
-      org: 'metromedics',
-      project: 'javascript-react'
-    })
+    tsconfigPaths()
+    // sentryVitePlugin({
+    //   org: 'metromedics',
+    //   project: 'javascript-react'
+    // })
   ],
 
   build: {
-    sourcemap: true,
+    sourcemap: false,
     outDir: 'dist',
     emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
-  },
-
-  // Asegurar que el servidor de desarrollo funcione correctamente
-  server: {
-    port: 3000,
-    open: true
+    chunkSizeWarningLimit: 2000
   }
 })
